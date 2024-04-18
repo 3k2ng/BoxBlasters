6edef2522fc11faf80b9e1b5b410be19a633e444
3k2ng <tung09102002@gmail.com>
2024-04-17 23:08:38 -0600

    now lets try to complete this thing

--- Source/BoxBlasters/Public/Libraries/PlannerUtils.h
+++ Source/BoxBlasters/Public/Libraries/PlannerUtils.h
@@ -11,8 +11,6 @@ struct FRobotPlan
 {
 	GENERATED_BODY()
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	int32 Cost;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TArray<FRobotTask> TaskList;
 };
 
@@ -29,8 +27,37 @@ struct FMaybePlan
 	static FMaybePlan Just(const FRobotPlan& InPlan) { return {true, InPlan}; }
 };
 
+USTRUCT(BlueprintType)
+struct FMapState
+{
+	GENERATED_BODY()
+	UPROPERTY()
+	TArray<ETileType> TileMap;
+	UPROPERTY()
+	TArray<EBombType> BombTypeMap;
+	UPROPERTY()
+	TArray<int32> BombPowerMap;
+	FMapState() {};
+	explicit FMapState(const AArmedBomber* Bomber);
+	TArray<ETileState> TileState() const;
+	TArray<FTile> GetBombedTiles(FTile BombTile, int32 BombPower) const;
+	FMapState DamageTiles(TArray<FTile> ToDamage) const;
+	FMapState PlaceBomb(FTile BombTile, EBombType BombType, int32 BombPower) const;
+	FMapState DetonateBomb(FTile Target) const;
+};
+
+struct FBombingPathFindStep
+{
+	int32 Cost;
+	FTile Location;
+	FMapState MapState;
+	TArray<FRobotTask> TaskList;
+};
+
 UCLASS()
 class BOXBLASTERS_API UPlannerUtils : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
+	UFUNCTION(BlueprintPure)
+	static FRobotPlan PlanToReach(const AArmedBomber* Bomber, const FTile Target);
 };
